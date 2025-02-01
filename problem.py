46d220be3e5ac42755b45d91f9a705e95d8e677e
Hamdi Burak Usul <busul@gams.com>
2025-02-01 18:30:27 +0100

    hello world

---
+++ problem.py
@@ -0,0 +1,145 @@
+from dataclasses import dataclass
+import sys
+import gamspy as gp
+import numpy as np
+
+
+coords = tuple[int, int]
+
+
+@dataclass
+class ProblemInput:
+    num_blocks: int
+    block_lens: list[int]
+    min_len: int
+    max_len: int
+    letters: set[str]
+    matches: list[tuple[int, int, int, int]]
+    blocks: list[tuple[coords, coords]]
+
+
+def main(problem: ProblemInput):
+    min_len = problem.min_len
+    max_len = problem.max_len
+    letters = problem.letters
+    num_blocks = problem.num_blocks
+    matches = [(str(mm) for mm in m) for m in problem.matches]
+
+    vocabulary = []
+
+    with open("american-english", "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if len(line) == 1:
+                continue
+
+            if len(line) > max_len or len(line) < min_len:
+                continue
+
+            if line.endswith("'s"):
+                continue
+
+            if set(line) <= letters:
+                vocabulary.append(line)
+
+    letters = list(letters)
+
+    m = gp.Container()
+
+    i = gp.Set(
+        m, "i", records=range(1, max_len + 1), description="set for letter places"
+    )
+
+    j = gp.Set(
+        m,
+        name="j",
+        records=range(1, num_blocks + 1),
+        description="Number of blocks in the puzzle",
+    )
+
+    ii = gp.Alias(m, "ii", alias_with=i)
+    jj = gp.Alias(m, "jj", alias_with=j)
+
+    common = gp.Set(
+        m,
+        name="common",
+        domain=[j, j, i, i],
+        description="Set indicating which char should be common",
+        records=matches,
+    )
+
+    k = gp.Set(
+        m, name="k", records=vocabulary, description="Words that we can choose from"
+    )
+
+    kk = gp.Alias(m, name="kk", alias_with=k)
+
+    l = gp.Set(m, name="l", records=letters, description="Letters that are allowed")
+
+    LB = gp.Parameter(
+        m,
+        domain=[j],
+        description="Length of the block j",
+        records=np.array(problem.block_lens),
+    )
+
+    wlens = np.array([len(x) for x in vocabulary])
+
+    LW = gp.Parameter(
+        m, name="LW", domain=[k], description="Length of words", records=wlens
+    )
+
+    wlet = gp.Parameter(m, name="wlet", domain=[k, i, l])
+
+    for word in vocabulary:
+        for ind, cha in enumerate(word):
+            wlet[word, str(ind + 1), cha] = 1
+
+    w = gp.Variable(
+        m,
+        name="w",
+        domain=[j, k],
+        type="binary",
+        description="which word is assigned to which place",
+    )
+
+    # allow only correct placing
+    w.fx[j, k].where[LW[k] != LB[j]] = 0
+
+    # assign words to every position
+    eq1 = gp.Equation(m, domain=[j])
+    eq1[j] = gp.Sum(k, w[j, k]) == 1
+
+    # use a word atmost once
+    eq2 = gp.Equation(m, domain=[k])
+    eq2[k] = gp.Sum(j, w[j, k]) <= 1
+
+    big_M = 3
+    # use common letters
+    eq3_1 = gp.Equation(m, domain=[j, jj, i, ii, l, k, kk])
+    eq3_1[j, jj, i, ii, l, k, kk].where[common[j, jj, i, ii]] = (
+        wlet[k, i, l] + (2 - w[j, k] - w[jj, kk]) * big_M >= wlet[kk, ii, l]
+    )
+
+    eq3_2 = gp.Equation(m, domain=[j, jj, i, ii, l, k, kk])
+    eq3_2[j, jj, i, ii, l, k, kk].where[common[j, jj, i, ii]] = (
+        wlet[k, i, l] <= wlet[kk, ii, l] + (2 - w[j, k] - w[jj, kk]) * big_M
+    )
+
+    model = gp.Model(
+        m,
+        equations=m.getEquations(),
+        problem="mip",
+    )
+
+    result = model.solve(output=sys.stdout)
+
+    data = w.records
+    data = data[data["level"] == 1]
+
+    summary = []
+    for index, row in data.iterrows():
+        summary.append((row["j"], row["k"]))
+
+    return result, summary
---
+++ ui.py
@@ -0,0 +1,227 @@
+import uuid
+import numpy as np
+import streamlit as st
+import itertools
+from problem import main, ProblemInput
+from functools import partial
+
+
+coords = tuple[int, int]
+
+
+def get_block_len(block: tuple[coords, coords]):
+    h_diff = block[1][0] - block[0][0]
+    w_diff = block[1][1] - block[0][1]
+    return max(w_diff, h_diff) + 1
+
+
+def get_block_letter(block: tuple[coords, coords], coord: coords):
+    h_diff = coord[0] - block[0][0]
+    w_diff = coord[1] - block[0][1]
+    return max(w_diff, h_diff) + 1
+
+
+def get_block_horizontal(values: np.ndarray):
+    h, w = values.shape
+    h_blocks = []
+    for ih in range(h):
+        state = None
+        for iw in range(w):
+            observation = values[ih, iw]
+            if observation:
+                if state is None:
+                    state = (ih, iw)
+                else:
+                    continue
+            else:
+                if state is None:
+                    continue
+                else:
+                    if iw != state[1] + 1:  # don't allow blocks with size 1
+                        h_blocks.append((state, (ih, iw - 1)))
+
+                    state = None
+
+        if state is not None and iw != state[1] + 1:
+            h_blocks.append((state, (ih, iw)))
+
+    return h_blocks
+
+
+def get_block_vertical(values: np.ndarray):
+    h, w = values.shape
+    v_blocks = []
+    for iw in range(w):
+        state = None
+        for ih in range(h):
+            observation = values[ih, iw]
+            if observation:
+                if state is None:
+                    state = (ih, iw)
+                else:
+                    continue
+            else:
+                if state is None:
+                    continue
+                else:
+                    if ih != state[0] + 1:  # don't allow blocks with size 1
+                        v_blocks.append((state, (ih - 1, iw)))
+
+                    state = None
+
+        if state is not None and ih != state[0] + 1:
+            v_blocks.append((state, (ih, iw)))
+
+    return v_blocks
+
+
+def detect_structure(values: np.ndarray) -> ProblemInput:
+    # blocks are numbered first horizontal blocks then vertical ones
+    h_blocks = get_block_horizontal(values)
+    v_blocks = get_block_vertical(values)
+    blocks = [*h_blocks, *v_blocks]
+    num_blocks = len(blocks)
+
+    block_lens = [get_block_len(x) for x in blocks]
+    min_len = min(block_lens)
+    max_len = max(block_lens)
+
+    matches = []
+    block_map = {}
+    for i, block in enumerate(blocks):
+        for ih in range(block[0][0], block[1][0] + 1):
+            for iw in range(block[0][1], block[1][1] + 1):
+                if (ih, iw) not in block_map:
+                    block_map[(ih, iw)] = i  # 1-indexed
+                else:
+                    first = block_map[(ih, iw)]
+                    second = i
+                    first_letter = get_block_letter(blocks[first], (ih, iw))
+                    second_letter = get_block_letter(block, (ih, iw))
+                    matches.append((first + 1, second + 1, first_letter, second_letter))
+
+    print(matches)
+    print("Number of block:", num_blocks)
+    print("Block lens:", block_lens)
+    print("Min len:", min_len)
+    print("Max len:", max_len)
+    print(h_blocks)
+    print(v_blocks)
+
+    return ProblemInput(
+        num_blocks=num_blocks,
+        block_lens=block_lens,
+        min_len=min_len,
+        max_len=max_len,
+        letters={"c", "a", "m", "p", "u", "l"},
+        matches=matches,
+        blocks=blocks,
+    )
+
+row1 = "abcdefgh"
+row2 = "ijklmnop"
+row3 = "qrstuvwxyz"
+
+letters_selected = {}
+
+st.header("Letters")
+
+cols1 = st.columns(10)
+cols2 = st.columns(10)
+cols3 = st.columns(10)
+
+for i, r in enumerate(row1):
+    with cols1[i]:
+        letters_selected[r] = st.checkbox(r, key=f"{r}")
+
+for i, r in enumerate(row2):
+    with cols2[i]:
+        letters_selected[r] = st.checkbox(r, key=f"{r}")
+
+for i, r in enumerate(row3):
+    with cols3[i]:
+        letters_selected[r] = st.checkbox(r, key=f"{r}")
+
+letters_selected = {k for k in letters_selected if letters_selected[k]}
+
+st.write("\n")
+
+
+row_count = 10
+col_count = 10
+
+def toggle_state(row, i):
+    st.session_state[(row, i)] = not st.session_state[(row, i)]
+
+st.header("Problem Structure")
+
+for row in range(row_count):
+    cols = st.columns(10)
+    for i, col in enumerate(cols):
+        if (row, i) not in st.session_state:
+            st.session_state[(row, i)] = False
+
+        with col:
+            color = "primary" if st.session_state[(row, i)] else "secondary"
+            st.button(
+                key=f"{row}x{i}",
+                label=" ",
+                on_click=partial(toggle_state, row, i),
+                type=color,
+                use_container_width=True,
+            )
+
+
+st.header("Solution")
+
+
+if "solution" not in st.session_state:
+    st.session_state["solution"] = {}
+
+if st.button("Solve"):
+    values = []
+    for row in range(row_count):
+        temp = []
+        for col in range(col_count):
+            temp.append(st.session_state[(row, col)])
+
+        values.append(temp)
+
+    values = np.array(values)
+
+    problem = detect_structure(values)
+    problem.letters = letters_selected
+
+    result, summary = main(problem)
+
+    new_solution = {}
+    for index, word in summary:
+        index = int(index)
+        block = problem.blocks[index - 1]
+        letter_index = 0
+        for ih in range(block[0][0], block[1][0] + 1):
+            for iw in range(block[0][1], block[1][1] + 1):
+                l = word[letter_index]
+                letter_index += 1
+                new_solution[(ih, iw)] = l
+
+    st.session_state["solution"] = new_solution
+
+    st.write(result)
+
+    for row in range(row_count):
+        cols = st.columns(10)
+        for i, col in enumerate(cols):
+            with col:
+                color = "primary" if st.session_state[(row, i)] else "secondary"
+                label = " "
+                if (row, i) in st.session_state["solution"]:
+                    label = st.session_state["solution"][(row, i)]
+
+                st.button(
+                    key=f"sol_{row}x{i}",
+                    label=label,
+                    type=color,
+                    use_container_width=True,
+                    disabled=True,
+                )
